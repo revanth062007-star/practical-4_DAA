e20a5adf246b8f64c36a2d95e0111fe601fde8d1
revanth062007-star <revanth062007@gmail.com>
2026-08-06 17:18:20 +0530

    Implement iterative factorial function in C++

---
+++ factorial using  iterative and recursive.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+long long factorial(int n) {
+    long long result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int num = 5;
+    std::cout << "Factorial of " << num << " is: " << factorial(num) << std::endl;
+    return 0;
+}
